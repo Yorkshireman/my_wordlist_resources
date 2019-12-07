69d3518656aa73373c9a3b9bcd3527c58d114262
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-07 01:07:03 +0000

    make ids private

--- app/controllers/wordlists_controller.rb
+++ app/controllers/wordlists_controller.rb
@@ -7,11 +7,13 @@ class WordlistsController < ApplicationController
     user_id = decode_token(token)[0]['user_id']
     wordlist = Wordlist.new(user_id: user_id)
     if wordlist.save
-      token = generate_token({ exp: (Time.now + 1800).to_i, wordlist_id: wordlist.id })
+      token = generate_token(user_id, wordlist_id)
       response.status = 201
       render json: {
         data: {
           token: token
+          type: 'wordlist',
+          attributes: {}
         }
       }
     end
@@ -20,24 +22,21 @@ class WordlistsController < ApplicationController
   def show
     token = request.headers['Authorization'].split(' ').last
     decoded_token = decode_token(token)[0]
-    unless decoded_token['user_id'] || decoded_token['wordlist_id']
+    user_id = decoded_token['user_id']
+    wordlist_id = decoded_token['wordlist_id']
+    unless user_id || wordlist_id
       render_error_response(400, 'Invalid token')
     end
 
-    wordlist = decoded_token['user_id'] ? Wordlist.find_by!(user_id: decoded_token['user_id']) : Wordlist.find(decoded_token['wordlist_id'])
-    serialised_wordlist = JSON.parse(wordlist.to_json).symbolize_keys
+    wordlist = wordlist_id ? Wordlist.find(wordlist_id) : Wordlist.find_by!(user_id: user_id)
+    # serialised_wordlist = JSON.parse(wordlist.to_json).symbolize_keys
 
-    generate_token({ exp: (Time.now + 1800).to_i, wordlist_id: serialised_wordlist[:id] }).then do |token|
+    generate_token(user_id, wordlist_id).then do |token|
       render json: {
         data: {
           token: token,
           type: 'wordlist',
-          id: serialised_wordlist[:id],
-          attributes: {
-            created_at: serialised_wordlist[:created_at],
-            updated_at: serialised_wordlist[:updated_at],
-            user_id: serialised_wordlist[:user_id]
-          }
+          attributes: {}
         }
       }
     end
--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -5,8 +5,8 @@ module TokenHelper
     JWT.decode(token, ENV['JWT_SECRET_KEY'], true, { algorithm: 'HS256' })
   end
 
-  def generate_token(payload)
+  def generate_token(user_id, wordlist_id)
     secret_key = ENV['JWT_SECRET_KEY']
-    JWT.encode(payload, secret_key, 'HS256')
+    JWT.encode({ exp: (Time.now + 1800).to_i, user_id: user_id, wordlist_id: wordlist_id }, secret_key, 'HS256')
   end
 end
