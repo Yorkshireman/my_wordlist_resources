aa448552d76db84fe214c41090a62a52c924101b
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-07 19:11:47 +0000

    write some POST /wordlists request specs

--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -5,7 +5,7 @@ module TokenHelper
     JWT.decode(token, ENV['JWT_SECRET_KEY'], true, { algorithm: 'HS256' })
   end
 
-  def generate_token(user_id, wordlist_id)
+  def generate_token(user_id, wordlist_id = nil)
     secret_key = ENV['JWT_SECRET_KEY']
     JWT.encode({ exp: (Time.now + 1800).to_i, user_id: user_id, wordlist_id: wordlist_id }, secret_key, 'HS256')
   end
--- spec/controllers/wordlists_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe WordlistsController, type: :controller do
-
-end
