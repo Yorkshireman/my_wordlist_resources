4ab824cfe0c7c1bc527de90b0af60a99afa41c57
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-02-07 18:44:24 +0000

    add create wordlist_entry route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'wordlist',         to: 'wordlists#show'
-  get 'wordlist_entries', to: 'wordlist_entries#index'
-  post 'wordlists',       to: 'wordlists#create'
+  get 'wordlist',           to: 'wordlists#show'
+  get 'wordlist_entries',   to: 'wordlist_entries#index'
+  post 'wordlist_entries',  to: 'wordlist_entries#create'
+  post 'wordlists',         to: 'wordlists#create'
 end
