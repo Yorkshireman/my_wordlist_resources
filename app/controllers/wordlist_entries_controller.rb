15e3b3fafdcc89952e926ce47425534611634089
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-02-07 18:45:12 +0000

    controller create wordlist_entry

--- app/controllers/wordlist_entries_controller.rb
+++ app/controllers/wordlist_entries_controller.rb
@@ -41,21 +41,32 @@ class WordlistEntriesController < ApplicationController
   end
 
   def create
-    token = request.headers['Authorization'].split(' ').last
-    decoded_token = decode_token(token)[0]
-    wordlist_id = decoded_token['wordlist_id'] # maybe use tap or then here
+    # token = request.headers['Authorization'].split(' ').last
+    # decoded_token = decode_token(token)[0]
+    # wordlist_id = decoded_token['wordlist_id'] # maybe use tap or then here
+    wordlist_id = '7663812f-3358-43d6-a58a-f1859b60763f'
 
     unless wordlist_id
       return render_error_response(400, 'Invalid token - missing wordlist id')
     end
 
     wordlist = Wordlist.find(wordlist_id)
+    # for now assume word does not exist yet
+    word = Word.create(name: params['wordlist_entry']['word'])
+    wordlist_entry = WordlistEntry.create(word_id: word.id, wordlist_id: wordlist_id, description: params['wordlist_entry']['description'])
+
     token = generate_token(wordlist.user_id, wordlist.id)
     render json: {
       data: {
         token: token,
-        type: 'wordlist',
+        type: 'wordlist_entry',
         attributes: {
+          id: wordlist_entry.id,
+          word: {
+            id: word.id,
+            name: word.name
+          },
+          description: wordlist_entry.description
         }
       }
     }
