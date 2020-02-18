b475b2d156bc4264e84590968dc4b8eee11a0bf3
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-02-18 21:43:59 +0000

    update seeds.rb with user ids from authentication-server stage db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,7 @@ def generate_random_sentence
   shuffled_array[0, random_length].join(' ')
 end
 
-uuids = []
-20.times do
-  uuids << SecureRandom.uuid
-end
+uuids = ['1c6b5a3b-290c-46c8-8afe-1f7714eb322d', 'a368aedc-8ab4-4d3d-a251-9be58c72754d', '91e86206-81ac-45a7-924d-886d4f0f4d53', '6108cd63-65a9-418e-b308-646d37318bd7', 'ca702f40-4406-4d47-954d-69bf091d2457', '198ff935-1b1d-471b-809f-91cd066be552', '2fd9e429-45b0-445e-9211-328d3fd96631', '1ebef7c8-1ccf-464c-801a-a9d96ca3be73', 'e37f5da6-23e7-499e-af56-7c4e55f20b42', 'a725f929-070f-4022-a19d-b369652ac6d0', 'cbe1b489-f162-4bb4-8ee0-7fb0287e4ebc', '558be55b-8200-4457-a25d-3113370dc42a', '2acc19ce-038e-43f8-8146-333daa4e24bc', '56aaad42-118e-4d82-aa76-2b8bf5ce4a6a', '2cd13ffd-30bb-47d1-ac1b-51a7057142b4', '8410373b-3491-4846-9bc4-f49284c3c4bb', '6efafcbc-4609-48fa-82b4-b8a3e357fd5c', 'be00a69f-3ec8-4d6b-9ef5-bb6e33f04068', 'a03fe91f-17d9-41e7-aeaf-89826602b1b4', '43fee27d-f478-44ec-afd8-33aca61efce1']
 
 # create Wordlists
 uuids.each { |uuid| Wordlist.create(user_id: uuid) }
