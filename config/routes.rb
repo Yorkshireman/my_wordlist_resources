093ba2fc964f594ed5ccda575961c609666560f7
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-02-20 22:59:47 +0000

    remove create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'wordlist',           to: 'wordlists#show'
   get 'wordlist_entries',   to: 'wordlist_entries#index'
-  post 'wordlist_entries',  to: 'wordlist_entries#create'
   post 'wordlists',         to: 'wordlists#create'
 end
