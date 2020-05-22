b72d4eeacb5c4ccc9766647d7737f9b139f668b7
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-05-22 21:41:01 +0100

    reverse WordlistEntries order

--- app/controllers/wordlist_entries_controller.rb
+++ app/controllers/wordlist_entries_controller.rb
@@ -4,43 +4,22 @@ class WordlistEntriesController < ApplicationController
   include TokenHelper
 
   def create
-    if wordlist_entry_params[:word][:id]
-      @word = Word.find(wordlist_entry_params[:word][:id])
-    else
-      @word = Word.find_by(wordlist_entry_params[:word])
-    end
-
-    if @word.nil?
-      @word = Word.create(wordlist_entry_params[:word])
-    end
+    word = find_or_create_word(wordlist_entry_params)
 
     get_wordlist_id_from_headers.then do |wordlist_id|
-      wordlist_entry = create_wordlist_entry(wordlist_id)
+      wordlist_entry = create_wordlist_entry(wordlist_id, word)
       token = Wordlist.find(wordlist_id).then { |wl| generate_token(wl.user_id, wl.id) }
+
       render json: {
         data: {
           token: token,
           type: 'wordlist-entry',
           id: wordlist_entry.id,
-          attributes: {
-            description: wordlist_entry.description,
-            word: {
-              id: @word.id,
-              name: @word.name,
-              wordlist_ids: @word.wordlist_ids
-            }
-          }
+          attributes: parse_wordlist_entry(wordlist_entry, word)
         }
       },
       status: :created
     end
-
-    rescue ActiveRecord::RecordNotFound => e
-      render_error_response(404, e)
-    rescue JWT::DecodeError => e
-      render_error_response(400, e)
-    rescue ActionController::ParameterMissing => e
-      render_error_response(400, e)
   end
 
   def index
@@ -50,44 +29,63 @@ class WordlistEntriesController < ApplicationController
     end
 
     wordlist = Wordlist.find(wordlist_id)
-    wordlist_entries = wordlist.wordlist_entries.reverse.map do |wordlist_entry|
-      {
-        attributes: {
-          word: {
-            id: wordlist_entry.word.id,
-            name: wordlist_entry.word.name,
-            wordlist_ids: wordlist_entry.word.wordlist_ids
-          },
-          description: wordlist_entry.description
-        }
-      }
-    end
+    wordlist_entries = wordlist.wordlist_entries.reverse
 
     generate_token(wordlist.user_id, wordlist.id).then do |token|
       render json: {
         data: {
           token: token,
-          wordlist_entries: wordlist_entries
+          wordlist_entries: parse_wordlist_entries(wordlist_entries)
         }
       }
     end
-
-    rescue ActiveRecord::RecordNotFound => e
-      render_error_response(404, e)
-    rescue JWT::DecodeError => e
-      render_error_response(400, e)
   end
 
   private
 
-  def create_wordlist_entry(wordlist_id)
-    WordlistEntry.create(wordlist_id: wordlist_id, word_id: @word.id, description: wordlist_entry_params[:description])
+
+  def create_wordlist_entry(wordlist_id, word)
+    WordlistEntry.create(wordlist_id: wordlist_id, word_id: word.id, description: wordlist_entry_params[:description])
+  end
+
+  def find_or_create_word(wordlist_entry_params)
+    if wordlist_entry_params[:word][:id]
+      word = Word.find(wordlist_entry_params[:word][:id])
+    else
+      word = Word.find_by(wordlist_entry_params[:word])
+    end
+
+    if word.nil?
+      word = Word.create(wordlist_entry_params[:word])
+    end
+
+    word
   end
 
   def get_wordlist_id_from_headers
     request.headers['Authorization'].split(' ').last.then do |token|
       decode_token(token)[0]['wordlist_id']
     end
+  end
+
+  def parse_wordlist_entries(wordlist_entries)
+    wordlist_entries.map do |wordlist_entry|
+      {
+        attributes: parse_wordlist_entry(wordlist_entry)
+      }
+    end
+  end
+
+  def parse_wordlist_entry(wordlist_entry, word = nil)
+    wordlist_entry_word = word || wordlist_entry.word
+    {
+      word: {
+        id: wordlist_entry_word.id,
+        name: wordlist_entry_word.name,
+        wordlist_ids: wordlist_entry_word.wordlist_ids
+      },
+      description: wordlist_entry.description
+    }
   end
 
   def render_error_response(status, message)
