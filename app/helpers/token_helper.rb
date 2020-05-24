d3c04b9224c11473f695f6654954a2ce9c6be7a8
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-05-24 23:28:40 +0100

    remove wordlist_id from token in wordlists_controller

--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -5,12 +5,12 @@ module TokenHelper
     JWT.decode(token, ENV['JWT_SECRET_KEY'], true, { algorithm: 'HS256' })
   end
 
-  def generate_token(user_id, wordlist_id = nil)
+  def generate_token(user_id)
     secret_key = ENV['JWT_SECRET_KEY']
     payload = if ENV['RAILS_ENV'] == 'test'
-                { user_id: user_id, wordlist_id: wordlist_id }
+                { user_id: user_id }
               else
-                { exp: (time_now + 1800).to_i, user_id: user_id, wordlist_id: wordlist_id }
+                { exp: (time_now + 1800).to_i, user_id: user_id }
               end
 
     JWT.encode(payload, secret_key, 'HS256')
