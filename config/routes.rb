389401ca60c6b018aec701bd2d30cc150a1de813
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-07-13 23:57:13 +0100

    sort out spacing in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'wordlist',                                                       to: 'wordlists#show'
-  post 'wordlists',                                                     to: 'wordlists#create'
+  get 'wordlist',   to: 'wordlists#show'
+  post 'wordlists', to: 'wordlists#create'
   resources :wordlist_entries, only: [:index, :create] do
     resources :categories, only: [:create]
   end
