199f17f664e81167b87139b6c5e7159659548448
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-07-17 20:34:25 +0100

    refactor

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -20,50 +20,55 @@ RSpec.describe 'POST /wordlist_entries/:wordlist_entry_id/categories', type: :re
   let(:wordlist) { Wordlist.create(user_id: user_id) }
   let(:wordlist_entry) { WordlistEntry.create(word_id: word.id, wordlist_id: wordlist.id) }
 
-  before :each do
-    params = {
-      categories: [
-        { id: category_id, name: category_name }
-      ]
-    }
 
-    post "/wordlist_entries/#{wordlist_entry.id}/categories", params: params.to_json, headers: headers
-  end
+  context 'when the WordlistEntry has no categories yet' do
+    let(:params) do
+      {
+        categories: [
+          { id: category_id, name: category_name }
+        ]
+      }
+    end
 
-  it 'adds a Category to a WordlistEntry' do # improve test name
-    expect(wordlist_entry.categories.first.id).to eq(category_id)
-    expect(wordlist_entry.categories.first.name).to eq(category_name)
-  end
-
-  it 'responds with 201 status' do
-    expect(response).to have_http_status(201)
-  end
-
-  describe 'body' do
     before :each do
-      @body = JSON.parse(response.body).deep_symbolize_keys
+      post "/wordlist_entries/#{wordlist_entry.id}/categories", params: params.to_json, headers: headers
     end
 
-    it 'has type' do
-      expect(@body[:data][:type]).to eq('wordlist-entry')
+    it 'adds a Category to a WordlistEntry' do # improve test name
+      expect(wordlist_entry.categories.first.id).to eq(category_id)
+      expect(wordlist_entry.categories.first.name).to eq(category_name)
     end
 
-    it 'has id' do
-      expect(@body[:data][:id]).to eq(wordlist_entry.id)
+    it 'responds with 201 status' do
+      expect(response).to have_http_status(201)
     end
 
-    it 'has categories' do
-      expect(@body[:data][:attributes][:categories]).to eq(
-        [{ id: category_id, name: category_name }]
-      )
-    end
+    describe 'body' do
+      before :each do
+        @body = JSON.parse(response.body).deep_symbolize_keys
+      end
 
-    it 'has a token' do
-      expect(@body[:data][:token]).to eq(generate_token(user_id))
+      it 'has type' do
+        expect(@body[:data][:type]).to eq('wordlist-entry')
+      end
+
+      it 'has id' do
+        expect(@body[:data][:id]).to eq(wordlist_entry.id)
+      end
+
+      it 'has categories' do
+        expect(@body[:data][:attributes][:categories]).to eq(
+          [{ id: category_id, name: category_name }]
+        )
+      end
+
+      it 'has a token' do
+        expect(@body[:data][:token]).to eq(generate_token(user_id))
+      end
     end
   end
 
-  context 'when some categories already exist' do
+  context 'when some categories already exist on the WordlistEntry' do
     let(:new_category_id) { SecureRandom.uuid }
     let(:new_category_name) { 'verb' }
     let(:params) do
